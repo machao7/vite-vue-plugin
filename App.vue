1948f4c7df0e0e36b5b8edd6c5d4f6f1dab9b09c
马超 <machao7@kingsoft.com>
2022-04-19 18:14:45 +0800

    init commit

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <button @click="pop">check component props</button>
+    <h4>dev mode: props includes render</h4>
+    <h4>build mode: props not includes render</h4>
+  </section>
+</template>
+
+<script>
+import { showPopup } from "./PopDemo.vue";
+
+export default {
+  setup() {
+    return {
+      pop() {
+        showPopup()
+      }
+    }
+  }
+}
+</script>
---
+++ PopDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    component
+  </section>
+</template>
+
+<script>
+const componentConfig = {
+  setup() {
+  }
+}
+
+export default componentConfig;
+
+export const showPopup = () => {
+  // in dev mode, componentConfig.render exist
+  // in build mode, componentConfig.render not exist
+  // this issue happened from @vitejs/plugin-vue@1.8.1
+  console.log("componentConfig'props ", Object.getOwnPropertyNames(componentConfig))
+  alert("PopDemo.vue config'props >> " + Object.getOwnPropertyNames(componentConfig).join(", "))
+}
+</script>
